cb4017cf7528236defad2ff58874b0c818a005d3
valtsu23 <valtteri.eklund@gmail.com>
2025-03-16 21:07:28 +0200

    Update shift_light_rpi5.py

--- shift_light_rpi5.py
+++ shift_light_rpi5.py
@@ -14,11 +14,12 @@ class Pi5Pixelbuf(adafruit_pixelbuf.PixelBuf):
     def _transmit(self, buf):
         neopixel_write(self._pin, buf)
 
-pixels = Pi5Pixelbuf(NEOPIXEL, NUM_PIXELS, auto_write=True, byteorder="BGR")
+pixels = Pi5Pixelbuf(NEOPIXEL, NUM_PIXELS, auto_write=False, byteorder="BGR")
 
 # Turn off all the leds
 def leds_off():
     pixels.fill((0, 0, 0))
+    pixels.show()
 
 # Blink frequency and timer
 BLINK = .1
@@ -34,43 +35,42 @@ def action(rpm, STEP, END, br):
     # Blinker
     if rpm > END + STEP:
         if t1 + BLINK < time.monotonic():
-            pixels.fill((0, br, br))
+            pixels.fill((0, br, 0))
         if t1 + BLINK * 2 < time.monotonic():
             pixels.fill((0, 0, 0))
             t1 = time.monotonic()
     # If shift light step is exceeded
     elif shift_changed != shift:
         # LED steps control
-        print("Shift:", shift)
-        print("Shift_changed:", shift_changed)
+        # print("Shift:", shift)
+        # print("Shift_changed:", shift_changed)
         if shift <= 3:
-            pixels[0] = (0, br, 0)
-            pixels[7] = (0, br, 0)
+            pixels[0] = (0, 0, br)
+            pixels[7] = (0, 0, br)
         else:
             pixels[0] = (0, 0, 0)
             pixels[7] = (0, 0, 0)
 
         if shift <= 2:
-            pixels[1] = (0, br, 0)
-            pixels[6] = (0, br, 0)
+            pixels[1] = (0, 0, br)
+            pixels[6] = (0, 0, br)
         else:
             pixels[1] = (0, 0, 0)
             pixels[6] = (0, 0, 0)
 
         if shift <= 1:
-            pixels[2] = (br, br, 0)
-            pixels[5] = (br, br, 0)
+            pixels[2] = (0, br, br)
+            pixels[5] = (0, br, br)
         else:
             pixels[2] = (0, 0, 0)
             pixels[5] = (0, 0, 0)
 
         if shift <= 0:
-            pixels[3] = (br, 0, 0)
-            pixels[4] = (br, 0, 0)
+            pixels[3] = (0, br, 0)
+            pixels[4] = (0, br, 0)
         else:
             pixels[3] = (0, 0, 0)
             pixels[4] = (0, 0, 0)
-
+    pixels.show()
     # Save the new state
     shift_changed = shift
-
